ffafda1be4235b9e1a823dd0a4acae0acdbc3416
chandubomma <85388886+chandubomma@users.noreply.github.com>
2024-04-08 11:24:35 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (50.61%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,18 +1,18 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        for(int i: students)q.offer(i);
-        int i=0,count=0;
-        while(i<sandwiches.length && count<=q.size()){
-            if(sandwiches[i]==q.peek()){
-                q.remove();
-                i++;
-                count=0;
-                continue;
-            }
-            q.offer(q.remove());
+        int i=0,j=0,count=0;
+        while(i<sandwiches.length && count<students.length-i){
+            while(students[j]==-1)j=(j+1)%students.length;
+           if(sandwiches[i]==students[j]){
+               students[j]=-1;
+               i++;
+               j=(j+1)%students.length;
+               count=0;
+               continue;
+           }
             count++;
+            j=(j+1)%students.length;
         }
-        return q.size();
+       return students.length-i;
     }
 }
