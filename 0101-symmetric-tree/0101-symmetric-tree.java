736e2ac46bfd6d28070965bdce4d012147b61670
chandubomma <85388886+chandubomma@users.noreply.github.com>
2024-07-15 10:08:15 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (5.21%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -16,7 +16,6 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root==null)return true;
-        invert(root.right);
         return checkTree(root.left,root.right);
     }
     
@@ -24,15 +23,7 @@ class Solution {
         if(root1==null && root2==null)return true;
         if(root1==null || root2==null)return false;
         if(root1.val!=root2.val)return false;
-        return checkTree(root1.left,root2.left)&&checkTree(root1.right,root2.right);
+        return checkTree(root1.left,root2.right)&&checkTree(root1.right,root2.left);
     }
     
-    public void invert(TreeNode root){
-        if(root==null)return;
-        invert(root.left);
-        invert(root.right);
-        TreeNode node = root.left;
-        root.left = root.right;
-        root.right = node;
-    }
 }
