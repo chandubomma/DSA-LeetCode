1adc1f10bf02b10699bdd8df422151fa74183363
chandubomma <85388886+chandubomma@users.noreply.github.com>
2024-07-26 09:23:06 +0530

    Time: 47 ms (18.30%), Space: 44.8 MB (21.78%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,39 +1,45 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>((p1,p2)->Integer.compare(p1.getValue(),p2.getValue()));
-        ArrayList<ArrayList<Pair<Integer,Integer>>> adj = new ArrayList<>();
-        int[] d = new int[n];
-        for(int i=0;i<n;i++){ 
-            adj.add(new ArrayList<>());
-        }
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++)adj.add(new ArrayList<>());
         for(int[] ed: edges){
             adj.get(ed[0]).add(new Pair(ed[1],ed[2]));
             adj.get(ed[1]).add(new Pair(ed[0],ed[2]));
         }
-        int[] counter = new int[n];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((p1,p2)->Integer.compare(p1.wt,p2.wt));
+        int[] d = new int[n];
+        int[] count = new int[n];
         for(int i=0;i<n;i++){
             pq.clear();
             Arrays.fill(d,Integer.MAX_VALUE);
-            pq.add(new Pair(i,0));
+            pq.offer(new Pair(i,0));
             d[i]=0;Pair p1;
             while(!pq.isEmpty()){
                 p1 = pq.poll();
-                for(Pair p2:adj.get((int)p1.getKey())){
-                    if((int)p1.getValue()+(int)p2.getValue()<d[(int)p2.getKey()]){
-                        d[(int)p2.getKey()] = (int)p1.getValue()+(int)p2.getValue();
-                        pq.add(new Pair((int)p2.getKey(),d[(int)p2.getKey()]));
+                for(Pair p2:adj.get(p1.city)){
+                    if(p1.wt+p2.wt<d[p2.city]){
+                        d[p2.city] = p1.wt+p2.wt;
+                        pq.offer(new Pair(p2.city,d[p2.city]));
                     }
                 }
             }
             for(int j=0;j<n;j++){
-                if(j!=i && d[j]<=distanceThreshold)counter[i]++;;
+                if(j!=i && d[j]<=distanceThreshold)count[i]++;
             }
         }
         int ans=0;
-        
         for(int i=1;i<n;i++){
-            if(counter[i]<=counter[ans])ans=i;
+            if(count[i]<=count[ans])ans=i;
         }
         return ans;
+    }
+    
+    class Pair{
+        int city;
+        int wt;
+        public Pair(int city,int wt){
+            this.city = city;
+            this.wt = wt;
+        }
     }
 }
