c83cee62998efb68ec2eb4c56754d7f2ba6ee52f
BOMMA CHANDU <85388886+chandubomma@users.noreply.github.com>
2024-10-21 19:45:30 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (48.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,34 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-        quickSort(nums,0,nums.length-1);
-        return;
-    }
-    
-    public void quickSort(int[] arr,int low,int high){
-       if(low>=high)return;
-        int partitionIndex = partition(arr,low,high);
-        quickSort(arr,low,partitionIndex-1);
-        quickSort(arr,partitionIndex+1,high);
-    }
-    
-    public int partition(int[] arr,int low,int high){
-        int pivot = arr[low];
-        int i=low+1;
-        int j=high;
-        while(i<=j){
-            while(i<=j && arr[i]<=pivot)i++;
-            while(j>=i && arr[j]>pivot)j--;
-            if(i>=j)break;
-            swap(arr,i,j);
+        int start=0,end=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0 && i>start){
+                swap(nums,start,i);
+                start++;i--;
+            }else if(nums[i]==2 && i<end){
+                swap(nums,i,end);
+                end--;i--;
+            }
         }
-        swap(arr,low,j);
-        return j;
     }
     
-    public void swap(int[] arr,int i,int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j]=temp;
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-    
 }
