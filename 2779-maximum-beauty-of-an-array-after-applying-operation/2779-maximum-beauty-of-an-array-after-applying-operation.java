75713b37d87a041fddb65ee5de6a684c05bff6c4
BOMMA CHANDU <85388886+chandubomma@users.noreply.github.com>
2024-12-11 09:17:56 +0530

    Time: 8 ms (89.01%), Space: 60.7 MB (20.79%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,15 +1,23 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
-       Arrays.sort(nums);
-        int i=0,j=0,max=0;
-        while(j<nums.length){
-            if(nums[j]-nums[i]<=2*k)j++;
-            else{
-                max=Math.max(max,j-i);
-                i++;
-            }
+       int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE,freq=0;
+        for(int i:nums){
+            max = Math.max(max,i);
+            min = Math.min(min,i);
         }
-         max=Math.max(max,j-i);
-        return max;
+        int[] pre = new int[max+1];
+        for(int i:nums)pre[i]++;
+        freq=pre[0];
+        for(int i=1;i<pre.length;i++){
+            freq = Math.max(freq,pre[i]);
+            pre[i]+=pre[i-1];
+        }
+        if(k==0)return freq;
+        int ans=0,count;
+        for(int i=min;i<=max;i++){
+            count = pre[i+k<=max?i+k:max]-((i-k>0)?pre[i-k-1]:0);
+            ans = Math.max(ans,count);
+        }
+        return ans;
     }
 }
