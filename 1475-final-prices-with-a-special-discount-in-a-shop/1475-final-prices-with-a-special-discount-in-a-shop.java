154428adec1ec34f4ff9067c37907bf8dc56b736
BOMMA CHANDU <85388886+chandubomma@users.noreply.github.com>
2024-12-18 08:24:24 +0530

    Time: 4 ms (40.47%), Space: 44.6 MB (6.13%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[prices.length];
+        for(int i=prices.length-1;i>=0;i--){
+            while(st.size()>0 && st.peek()>prices[i])st.pop();
+            if(st.size()>0){
+                ans[i] = prices[i]-st.peek();
+            }else ans[i] = prices[i];
+            st.push(prices[i]);
+        }
+        return ans;
+    }
+}
